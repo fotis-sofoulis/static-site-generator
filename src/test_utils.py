9666c6e32f2e453dfaf16c2ace6503f7beee8102
Fotis <fotis.sofoulis@gmail.com>
2025-05-27 14:48:16 +0300

    added split node delimeter funciton and tests

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,9 +1,9 @@
 import unittest
 from textnode import TextNode, TextType
-from utils import text_node_to_html_node
+from utils import text_node_to_html_node, split_nodes_delimiter
 
 
-class TestUtils(unittest.TestCase):
+class TestTextToHtml(unittest.TestCase):
     def test_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
@@ -47,6 +47,76 @@ class TestUtils(unittest.TestCase):
             "src": "https://example.com/image.png",
             "alt": "Alt text"
         })
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_bold_base_case(self):
+        nodes = [TextNode("Hello **world**", TextType.TEXT)]
+        new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("Hello ", TextType.TEXT),
+                TextNode("world", TextType.BOLD),
+            ]
+        )
+
+    def test_multiple_inline_delimiters(self):
+        nodes = [TextNode("This **is** a **test**", TextType.TEXT)]
+        new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This ", TextType.TEXT),
+                TextNode("is", TextType.BOLD),
+                TextNode(" a ", TextType.TEXT),
+                TextNode("test", TextType.BOLD),
+            ]
+        )
+
+    def test_invalid_delimiter(self):
+        nodes = [TextNode("Hello *world*", TextType.TEXT)]
+        with self.assertRaises(ValueError) as context:
+            split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        self.assertEqual(str(context.exception), "wrong delimiter")
+
+    def test_mismatched_delimiter_type(self):
+        nodes = [TextNode("Hello **world**", TextType.TEXT)]
+        with self.assertRaises(ValueError) as context:
+            split_nodes_delimiter(nodes, "**", TextType.ITALIC)
+        self.assertIn("Delimiter '**' does not match text type 'italic'", str(context.exception))
+
+    def test_unmatched_delimiter(self):
+        nodes = [TextNode("Hello **world", TextType.TEXT)]
+        with self.assertRaises(Exception) as context:
+            split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        self.assertEqual(str(context.exception), "unmatching delimiter")
+
+    def test_empty_text(self):
+        nodes = [TextNode("", TextType.TEXT)]
+        new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [])
+
+    def test_empty_parts(self):
+        nodes = [TextNode("****", TextType.TEXT)]
+        new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [])
+
+    def test_non_text_node_passthrough(self):
+        nodes = [
+            TextNode("This is plain", TextType.TEXT),
+            TextNode("This is bold", TextType.BOLD),
+            TextNode("This is italic", TextType.ITALIC),
+        ]
+        new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is plain", TextType.TEXT),
+                TextNode("This is bold", TextType.BOLD),
+                TextNode("This is italic", TextType.ITALIC),
+            ]
+        )
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -18,3 +18,40 @@ def text_node_to_html_node(text_node: TextNode):
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception("Incorrect Type")
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    if delimiter not in ["**", "_", "`"]:
+        raise ValueError("wrong delimiter")
+
+    delimiter_to_type = {
+        "**": TextType.BOLD,
+        "_": TextType.ITALIC,
+        "`": TextType.CODE,
+    }
+
+    if text_type != delimiter_to_type.get(delimiter):
+        raise ValueError(
+            f"Delimiter '{delimiter}' does not match text type '{text_type.value}'. "
+            f"Expected '{delimiter}' for '{delimiter_to_type[delimiter]}'."
+        )
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        parts = node.text.split(delimiter)
+        if len(parts) % 2 == 0:
+            raise Exception("unmatching delimiter")
+
+        for i, part in enumerate(parts):
+            if not part:
+                continue
+            if i % 2 == 0:
+                new_nodes.extend([TextNode(part, TextType.TEXT)])
+            else:
+                new_nodes.extend([TextNode(part, text_type)])
+
+    return new_nodes
