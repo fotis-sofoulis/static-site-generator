14c54fa2103f8822d52c445f31f9726116154af6
Fotis <fotis.sofoulis@gmail.com>
2025-05-31 04:04:00 +0300

    docs build

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import sys
 
 def main():
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
-    clean_copy("static/", "public/")
-    generate_pages_recursive("content/", "template.html", "public/", basepath)
+    clean_copy("static/", "docs/")
+    generate_pages_recursive("content/", "template.html", "docs/", basepath)
 
 
 if __name__ == "__main__":
